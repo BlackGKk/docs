accf93cc8433661b1bfd0c147f4fbc8f6c3f4878
nihaozeng <1934486445@qq.com>
2022-05-14 18:15:23 +0800

    pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,41 @@
+const moment = require('moment');
 module.exports = {
+  base: "/docs/",
   title: "Reol",
   description: "Reol的练习笔记",
   head: [
     ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
     ['meta', { name: 'author', content: 'Reol' }],
-    ['meta', { name: 'keywords', content: 'vuepress 介绍, vuepress 说明, Reol' }]
+    ['meta', { name: 'keywords', content: 'vuepress 介绍, vuepress 说明, Reol' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/icon-72x72.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp) => {
+          moment.locale("zh-cn")
+          return moment(timestamp).format("LLLL")
+        }
+      }
+    ],
+    [
+      '@vuepress/pwa', 
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        }
+     }
+    ]
   ],
   themeConfig: {
     lastUpdated: '更新时间',
